e0a5ffc8b648beb30d04cf9fd23bc9a22e43986b
bfusion <barbara.fusinska@gmail.com>
2018-03-11 19:03:56 +0000

    Added plot 3

--- plot3.R
+++ plot3.R
@@ -5,3 +5,6 @@ lines(power_data$DateTime, power_data$Sub_metering_1)
 lines(power_data$DateTime, power_data$Sub_metering_2, col="red")
 lines(power_data$DateTime, power_data$Sub_metering_3, col="blue")
 legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=c("black", "red", "blue"), lty=1, cex=0.4)
+
+dev.copy(png, "plot3.png")
+dev.off()
